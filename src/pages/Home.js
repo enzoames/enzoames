2c686a57dda1ab373b78951e27ffe5d6737d49f5
Enzo <e.amespizarro@gmail.com>
2019-10-04 00:50:46 -0400

    [enzoames] about me section in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,10 +29,17 @@ const AboutMe = styled.div`
   margin: 0 auto;
   padding: 60px;
   width: 1000px;
+  letter-spacing: 1px;
 
-  div {
+  > div {
     margin: 60px 0 30px 0;
     font-size: 18px;
+  }
+
+  > p {
+    line-height: 25px;
+    padding: 0 15px;
+    box-sizing: border-box;
   }
 `;
 
@@ -40,37 +47,59 @@ function Home() {
   return (
     <HomePage>
       <Intro>
-        Hi, I'm Enzo - a photographer turned software engineer based in New York
-        City. Currently building beautifully designed web applications at
+        Hi, I&apos;m Enzo - a photographer turned software engineer based in New
+        York City. Currently building beautifully designed web applications at
         Foursquare.
       </Intro>
       <PhotoMe src={ENZ} />
       <AboutMe>
         <div>Life Finds A Way</div>
         <p>
-          Lets begin where life began for me, but lets keep it short and simple.
-          Life was difficult - I know, pretty cliche but hear me out.
+          Let&apos;s begin where life began for me -- born and raised in Lima,
+          Peru. After 11 short years, I began my life in New York City. Each day
+          I grew thicker skin and made my way through the public education
+          system. And then there was college, where life was drained out of me
+          through homework. A thousand and one school nights later I graduated.
+          Now, I see sunshine and work as a software engineer.
         </p>
         <div>Futbol</div>
-        <p>
-          I had a dream once as a kid. It was to be the greatest. I have been
-          told that I had potential growing up. I never truly believed in myself
-          and lacked confidence.
-        </p>
+        <p>I love the the sport. Simple.</p>
         <div>Quotes</div>
+        <p>I love good quotes. Here are some I like, love, and live to.</p>
+        <p>Who are you when no one is watching?</p>
+        <p>Experience is the name we give to our mistakes</p>
+        <p>A system cannot fail those it was never designed to protect</p>
         <p>
-          I love good quotes. Quotes that I can live by and apply to my daily
-          life
+          You are an average of the five people you most spend your time with,
+          choose wisely
         </p>
+        <p>Teach me how to be harder than life and kinder than love</p>
+        <p>
+          Management is doing things right. Leadership is doing the right things
+        </p>
+        <p>Your future self is talking sh*t about you</p>
+        <p>Why does Socrates never cries and Jesus never laughs</p>
+        <p>There is no courage without the precense of fear</p>
+        <p>Let&apos;s make man kind</p>
+        <p></p>
         <div>Photography</div>
-        <p>so much here</p>
+        <p>
+          I love Photography -- good looking people, beautiful landscapes,
+          fruits on a table, motion, and street.
+        </p>
         <div>Inspiration</div>
         <p>
-          People that have made the most positive impact in my life have come
-          after graduating college. I drive my inspiration
+          My inspiration is driven from books, but primarily from the people
+          around me. I wouldn&apos;t be where I am today if it wasn&apos;t for
+          the people that have granted a helping hand to pull me forward. From
+          my family and handful of close friends, to the incredible collegues at
+          the places that I have worked. Thank you to you all.
         </p>
         <div>Software Engineering</div>
-        <p>so much here</p>
+        <p>
+          That&apos;s what I do now. Inspired by my brother who worked as a
+          software engineer at IBM in Peru. Gracias.
+        </p>
       </AboutMe>
     </HomePage>
   );
