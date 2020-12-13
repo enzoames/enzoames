3be52821667a3e409b09cef4f37f2a76b0d07e38
Enzo <e.amespizarro@gmail.com>
2020-12-13 18:51:32 -0500

    [ENZOAMES] new cover photo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@
 
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import ENZ from '../styles/img/ENZ_0884.jpg';
+import ENZ from '../styles/img/ENZ_1250.jpg';
 import media from '../styles/media';
 import { GA_URL } from '../utils/config';
 import Analytics from '../utils/Analytics';
@@ -28,21 +28,14 @@ const Intro = styled.h1`
 const PhotoMe = styled.img`
   width: 100%;
   height: 400px;
+  display: block;
+  padding: 0 30px;
   object-fit: cover;
-  object-position: 85% 30px;
-
-  ${media.xxs`
-    object-position: 89% 30px;    
-  `}
+  margin: 0 auto;
 
   ${media.md`
-    margin-bottom: 30px;
-    object-position: 0px -10px;
-  `}
-
-  ${media.lg`
-    margin-bottom: 30px;
-    object-position: 0px -40px;
+    width: 1000px;
+    padding: 0 60px;
   `}
 `;
 
@@ -95,16 +88,20 @@ function Home() {
   return (
     <>
       <Intro>
-        Hi, I&apos;m Enzo - a photographer turned software engineer based in New York City. Currently building
-        beautifully designed web applications at Foursquare.
+        Hi, I&apos;m Enzo - a photographer turned software engineer based in New York City.
+        Currently building beautifully designed web applications at Foursquare.
       </Intro>
-      <PhotoMe src={ENZ} alt="Enzo Ames photo profile headshot" title="Enzo Ames photo profile headshot" />
+      <PhotoMe
+        src={ENZ}
+        alt="Enzo Ames photo profile headshot"
+        title="Enzo Ames photo profile headshot"
+      />
       <AboutMe>
         <h2>Life Finds A Way</h2>
         <p>
-          Let&apos;s begin where life began for me - born and raised in Lima, Peru. After 11 short years, I moved to New
-          York City. Each day I grew a thicker skin and made my way through the public education system. Now, I see
-          sunshine and work as a software engineer.
+          Let&apos;s begin where life began for me - born and raised in Lima, Peru. After 11 short
+          years, I moved to New York City. Each day I grew a thicker skin and made my way through
+          the public education system. Now, I see sunshine and work as a software engineer.
         </p>
         <h2>Fútbol</h2>
         <p>I love the sport. Simple.</p>
@@ -118,7 +115,9 @@ function Home() {
             <i>Experience is the name we give to our mistakes</i>
           </li>
           <li>
-            <i>You are an average of the five people you most spend your time with, choose wisely</i>
+            <i>
+              You are an average of the five people you most spend your time with, choose wisely
+            </i>
           </li>
           <li>
             <i>Teach me how to be harder than life and kinder than love</i>
@@ -131,14 +130,20 @@ function Home() {
           </li>
         </ul>
         <h2>Photography</h2>
-        <p>I love photography - good looking people, beautiful landscapes, fruits on a table, motion, and street.</p>
+        <p>
+          I love photography - good looking people, beautiful landscapes, fruits on a table, motion,
+          and street.
+        </p>
         <h2>Inspiration</h2>
         <p>
-          My inspiration is primarily driven from books and the people around me - my family, a handful of close
-          friends, and the incredible collegues at the places that I have worked. Thank you all.
+          My inspiration is primarily driven from books and the people around me - my family, a
+          handful of close friends, and the incredible collegues at the places that I have worked.
+          Thank you all.
         </p>
         <h2>Software Engineering</h2>
-        <p>A career inspired by my brother who worked as a software engineer at IBM in Peru. Gracias.</p>
+        <p>
+          A career inspired by my brother who worked as a software engineer at IBM in Peru. Gracias.
+        </p>
       </AboutMe>
     </>
   );
